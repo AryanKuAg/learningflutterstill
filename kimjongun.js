5fff3b58aa08dfdec3d56f131b82a8dfce9b45a0
Aryan Agrawal <alemantrixaryan@gmail.com>
2021-10-04 04:53:53 +0545

    I told you its my daily routine

--- kimjongun.js
+++ kimjongun.js
@@ -1,8 +1,72 @@
-const fs = require('fs');
-const kiding = require('./utils.js');
+const yargs = require('yargs');
 
-const nothing = kiding(5, 2);
-console.log(nothing);
+// Adding a new note
+yargs.command({
+    command: "add",
+    description: "Adding a new note",
+    builder: {
+        title: {
+            description: "Your Note Title",
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            description: "Your Note Body",
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Your Note Title:', argv.title);
+        console.log('Your Note Body:', argv.body);
 
-// fs.writeFileSync("thisismadebynode.txt", "hahaha");
-// fs.appendFileSync('thisismadebynode.txt', "this is gonna be an appending text");
+    }
+});
+yargs.parse();
+
+
+
+
+
+
+
+
+
+
+
+
+// Removing a new note
+// yargs.command({
+//     command: "remove",
+//     description: "Remove your note",
+//     showInHelp: true,
+//     builder: {
+//         title: {
+//             description: 'to remove the note ja',
+//             showInHelp: true
+//         }
+//     },
+//     handler: function (argvskld) {
+//         console.log('Removing a new note', argvskld);
+//     }
+// });
+
+
+
+// // List your notes
+// yargs.command({
+//     command: "list",
+//     description: "Listing your notes",
+//     handler: function () {
+//         console.log('Listing your all notes');
+//     }
+// }).help.argv;
+
+// // Reading your new note
+// yargs.command({
+//     command: "read",
+//     description: "Reading a note",
+//     handler: function () {
+//         console.log('Reading your note');
+//     }
+// }).help.argv;
---
+++ learningaboutjson.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+// const human = {
+//     name: "Aryan",
+//     age: 18,
+//     gender: "male"
+// };
+// const humanJSON = JSON.stringify(human);
+
+// fs.writeFileSync('json-01.json', humanJSON);
+
+// const buffer = fs.readFileSync('json-01.json');
+// const buffertojson = buffer.toString();
+// const jsontobject = JSON.parse(buffertojson);
+// jsontobject.name = "Agrawal ji";
+// jsontobject.age = 19;
+// const justanother = JSON.stringify(jsontobject);
+// fs.writeFileSync('json-01.json', justanother);
