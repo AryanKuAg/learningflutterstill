2095acfbe2d726a43079bc14de0dd3da1a3eaad6
Aryan Agrawal <alemantrixaryan@gmail.com>
2021-10-05 05:29:37 +0545

    new day happy day

--- kimjongun.js
+++ kimjongun.js
@@ -1,5 +1,7 @@
 const yargs = require('yargs');
+const fs = require('fs');
 const learningaboutjson = require('./learningaboutjson.js');
+const chalk = require('chalk');
 
 // Adding a new note
 yargs.command({
@@ -49,22 +51,54 @@ yargs.command({
 
 
 
-// // List your notes
-// yargs.command({
-//     command: "list",
-//     description: "Listing your notes",
-//     handler: function () {
-//         console.log('Listing your all notes');
-//     }
-// }).help.argv;
+// List your notes
+yargs.command({
+    command: "list",
+    description: "Listing your notes",
+    handler: function () {
+        try {
+            const thing = fs.readFileSync('json-01.json')
+            const thingjson = thing.toString();
+            const ob = JSON.parse(thingjson)
+            console.log(chalk.yellow('Your notes are following:'))
+            ob.forEach(element => {
+                console.log(chalk.white.inverse(element.name), chalk.cyan.inverse(element.gender))
+            })
+        } catch (e) {
+            console.log('No notes found!!!')
+        }
+    }
+});
 
-// // Reading your new note
-// yargs.command({
-//     command: "read",
-//     description: "Reading a note",
-//     handler: function () {
-//         console.log('Reading your note');
-//     }
-// }).help.argv;
+// Reading your new note
+yargs.command({
+    command: "read",
+    description: "Reading a note",
+    builder: {
+        title: {
+            description: 'note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        try {
+            const allnotes = fs.readFileSync('json-01.json')
+            const notestojson = allnotes.toString()
+            const notestoobject = JSON.parse(notestojson)
+            const thenote = notestoobject.find(elem => elem.name === argv.title)
+            if (thenote) {
+                console.log(chalk.bold.yellow.inverse(thenote.name))
+                console.log(chalk.whiteBright(thenote.gender))
+
+            } else {
+                console.log(chalk.red.inverse('Nothing found like that!!!!'))
+
+            }
+        } catch (e) {
+            console.log(chalk.red.inverse('Nothing found!!!!'))
+        }
+    }
+});
 
 yargs.parse();
